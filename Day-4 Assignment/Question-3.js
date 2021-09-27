2f8e58bd7a67ad01c41c54360c67fbc8303de510
Chetan-charan <84982604+Chetan-charan@users.noreply.github.com>
2021-09-27 17:18:36 +0530

    Update Question-3.js

--- Day-4 Assignment/Question-3.js
+++ Day-4 Assignment/Question-3.js
@@ -1,18 +1,20 @@
 
 
+
 var getFlags = () => {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET","https://restcountries.com/v3/all");
+    xhr.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 
     xhr.responseType = "json";
 
     xhr.onload = () => {
         var arr = xhr.response;
        for(var country of arr){
-           console.log("Country:"+country.name.common);
+           console.log("Country:"+country.name);
            console.log("Region:"+country.region);
            console.log("Sub Region:"+country.subregion);
+           console.log("Population:"+country.population);
        }
     }
 
