da6d964d37a0cbef6cf01a3944c9f3001f04b9ff
Chetan-charan <84982604+Chetan-charan@users.noreply.github.com>
2021-10-27 22:19:16 +0530

    Update Question-2.js

--- Day-4 Assignment/Question-2.js
+++ Day-4 Assignment/Question-2.js
@@ -3,14 +3,14 @@
 var getFlags = () => {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET","https://restcountries.com/v3/all");
+    xhr.open("GET","https://restcountries.com/v3/all");                      //open xhr request to get all the countries
 
-    xhr.responseType = "json";
+    xhr.responseType = "json";                                               //response type json
 
     xhr.onload = () => {
-        var arr = xhr.response;
+        var arr = xhr.response;                                           
        for(var country of arr){
-           console.log(country.flags);
+           console.log(country.flags);                                        // flag of each country
        }
     }
 
